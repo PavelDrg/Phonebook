fb151de3ed747f3d57b40751f1876e445f5039c3
PavelDrg <pavel.dragan2@gmail.com>
2023-12-18 23:47:28 +0200

    Updated contacts card design

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,11 +1,14 @@
 <template>
-  <div>
-    <h1>CONTACTS</h1>
-    <div v-for="contact in contacts" :key="contact.id">
-      <p>{{ contact.name }}</p>
-      <p>{{ contact.email }}</p>
-    </div>
-  </div>
+  <v-card class="phonebook-card" v-for="contact in contacts" :key="contact.id">
+    <v-card-title>
+      {{ contact.first_name }} {{ contact.last_name }}
+    </v-card-title>
+    <v-card-subtitle>{{ contact.company }}</v-card-subtitle>
+    <v-card-text
+      >{{ contact.phone_number }} <br />
+      {{ contact.notes }}</v-card-text
+    >
+  </v-card>
 </template>
 
 <script setup>
@@ -14,4 +17,9 @@ const store = useStore();
 const contacts = store.state.contacts;
 </script>
 
-<style scoped></style>
+<style scoped>
+.phonebook-card {
+  width: 65vw;
+  margin: 2vh 0;
+}
+</style>
